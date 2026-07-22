8ea6baf45008f06eff1331ee9cd83ee037c84281
mendozalz <tu-email@example.com>
2026-07-21 22:05:08 -0500

    Refactor user search function and enhance user management endpoints

---
+++ for_loop.py
@@ -0,0 +1,38 @@
+############## Ciclo FOR LOOP ################
+
+# lista_nombre = ['edward','carlos','viviana']
+# tupla_notas = ('edward',30,4.5)
+# set_departamentos = {'ventas','compras','redes','it'}
+
+# for x in lista_nombre:
+#     print("/******Listas*******/")
+#     print(x)
+#     print("/*************/")
+
+# for y in tupla_notas:
+#     print("/******Tuplas*******/")
+#     print(y)
+#     print("/*************/")
+
+# for z in set_departamentos:
+#     print("/******Sets*******/")
+#     print(z)
+#     print("/*************/")
+
+
+############## Ciclo FOR RANGE ################
+
+# lista_nombre = ['edward','carlos','viviana']
+
+# for i in range(3):
+#     print(i, lista_nombre[i])
+
+# for x in lista_nombre:
+#     print(x)
+
+############## Ciclo FOR ENUMARATE ################
+
+lista_nombre = ['edward','carlos','viviana']
+
+for i, nombre in enumerate(lista_nombre):
+    print(i, nombre)
--- users.py
+++ users.py
@@ -14,12 +14,7 @@ user_list = [
     User(id=3, name="Che", age=18),
 ]
 
-
-@app.get("/users")
-async def users():
-    return user_list
-
-def return_user_id(id:int):
+def search_user_id(id:int):
     user_list_lambda = lambda user:user.id == id
     user_id = filter(user_list_lambda, user_list)
     try:
@@ -27,13 +22,50 @@ def return_user_id(id:int):
     except:
         return "Usuario no encontrado"
 
+@app.get("/users")
+async def users():
+    return user_list
+
 #Pathparams
 @app.get("/user/{id}")
 async def user(id:int):
-    return return_user_id(id)
-    
+    return search_user_id(id)
 
 #Queryparams
 @app.get("/user/")
 async def user(id:int):
-    return return_user_id(id)
+    return search_user_id(id)
+
+############# Method Post #################
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user_id(user.id)) == User:
+        return f"Error. El usuario ---{user.name}--- ya existe"
+    user_list.append(user)
+    return user
+
+############# Method Put #################
+
+@app.put("/user/")
+async def user(user: User):
+    found = False
+    for index, saved_user in enumerate(user_list):
+        if saved_user.id == user.id:
+            user_list[index] = user
+            found = True
+    if not found:
+        return {"Error: Usuario no actualizado"}
+    return user
+
+############# Method Delete #################
+
+@app.delete("/user/{id}")
+async def user(id:int):
+    found = False
+    for index, delete_user in enumerate(user_list):
+        if delete_user.id == id: # Siempre verificar el ".id"
+            del user_list[index]
+            found = True
+    if not found:
+        return {"El usuario no existe"}
